6ce1024fc4c42d3c05561800453caa880148b85c
rsingh0706 <rahulkherli007@gmail.com>
2026-03-12 17:27:49 +0530

    index value count

--- 05.array.sh
+++ 05.array.sh
@@ -2,8 +2,9 @@
 
 #Array
 
-myArray=( 1 3 10.7 7 hello "hey buddy!" )
-
+myArray=( 1 3 10.7 7 hello "Hello dear!" )
 echo "All the value in array are ${myArray[*]}"
-
 echo "Value are 5 index value ${myArray[4]}"
+
+#How to get lenth of array
+echo " No. of value, lenth of an array is ${#myArray[*]}"
