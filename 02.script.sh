7c83a11ee713530d441cb24416f805f8edbde494
rsingh0706 <rahulkherli007@gmail.com>
2026-03-12 15:24:59 +0530

    shell scripts variable

---
+++ 02.script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# single line comment
+
+echo "hello rahul"
+
+<<comment
+this
+is 
+multi
+line comments
+comment
+
---
+++ 03.variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this script file use varable value
+
+name=rahul
+age=25
+palce=jaipur
+
+echo "My name is $name and age $age and place $place"
+
+name=swetank
+
+echo "My name is $name"
